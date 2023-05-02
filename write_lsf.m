2be06b6fa41606955d65a17c5222e94b66f43177
Siqi Li <1990lisiqi@gmail.com>
2023-05-02 16:45:29 -0400

    Update write_lsf.m

--- write_lsf.m
+++ write_lsf.m
@@ -1,30 +1,33 @@
 %==========================================================================
-% Write the FVCOM spg input file (ASCII)
+% Write the FVCOM lsf input file (ASCII)
 %
-% input  : foutput   --- spg path and name
-%          spg_node  --- sponge center node id
-%          spg_R     --- sponge layer's affecting radius
-%          spg_f     --- Dampng coefficient 
+% input  : foutput      --- lsf path and name
+%          lsf_node_1st --- 1st array node id
+%          lsf_node_2nd --- 2nd array node id
+%          lsf_geo      --- thermal wind flow adjusting scaling in a range of [0 1]
+%          lsf_wdf      --- wind driven flow adjusting scaling in a range of [0 1]
 %          
-% spg file format:
-% Sponge Node Number = 5
-%   1   20000  0.01
-%   2   20000  0.01
-%   3   20000  0.01
-%   4   20000  0.01
-%   5   20000  0.01
+% lsf file format:
+% Longshore Flow Node Number =   23
+% # NODES MUST BE LISTED IN THE OFFSHORE DIRECTION
+% #     NODE(1st) NODE(2nd)  GEO       WD
+%   1        1      260      1.0      0.5
+%   2        2      259      1.0      0.2
+%   3        3      258      1.0      0.0
+%   4        4      257      1.0      0.0
+%   5        5      256      1.0      0.0
 %  ...
 %
 %
 % Siqi Li, SMAST
 % 2021-12-01
 %==========================================================================
-function [lsf_node_2nd, lsf_cell] = write_lsf(fout, fgrid, lsf_node_1st, lsf_node_2nd, lsf_geo, lsf_wdf)
+function write_lsf(fout, lsf_node_1st, lsf_node_2nd, lsf_geo, lsf_wdf)
 
 
 
-% Find out the related cells
-lsf_cell = find(all(ismember(fgrid.nv, [lsf_node_1st(:); lsf_node_2nd(:)]), 2));
+%% Find out the related cells
+%lsf_cell = find(all(ismember(fgrid.nv, [lsf_node_1st(:); lsf_node_2nd(:)]), 2));
 
 m = length(lsf_node_1st);
 
