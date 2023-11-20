864fa53d3581c4622981434c4376e6f3ffe1ea3b
Siqi Li <1990lisiqi@gmail.com>
2023-11-20 16:15:28 -0500

    Update add_nesting_hyw.m

--- add_nesting_hyw.m
+++ add_nesting_hyw.m
@@ -2,8 +2,8 @@
 % Add 'hyw' to the nesting boundary file
 % 
 % Input  : --- fin, original input nesting file
-%          --- fout, the new nesting file with hyw (optional)
-%          --- hyw, hydro-static vertical velocity (optional)
+%          --- fout, the new nesting file with hyw 
+%          --- hyw, hydro-static vertical velocity 
 %
 % Output : \
 % 
@@ -17,17 +17,14 @@
 % Updates:
 %
 %==========================================================================
-function add_nesting_hyw(fin, fout, varargin)
+function add_nesting_hyw(fin, fout, hyw)
 
-varargin = read_varargin(varargin, {'hyw'}, {0});
 
-if exist('fout', 'var')
-    copyfile(fin, fout);
-else
-    fout = fin;
+if ~strcmp(fin, fout)
+    disp('Copying file.')
+    copyfile(fin, fout)
+    disp('Copy finished.')
 end
-
-% f = f_load_grid(fout, 'xy');
 
 
 ncid = netcdf.open(fout, 'NC_WRITE');
@@ -37,12 +34,18 @@ node_dimid = netcdf.inqDimID(ncid, 'node');
 siglev_dimid = netcdf.inqDimID(ncid, 'siglev');
 time_dimid = netcdf.inqDimID(ncid, 'time');
 
+[~, node] = netcdf.inqDim(ncid, node_dimid);
+[~, kb] = netcdf.inqDim(ncid, siglev_dimid);
+[~, nt] = netcdf.inqDim(ncid, time_dimid);
 if (numel(hyw) == 1)
-    [~, node] = netcdf.inqDim(ncid, node_dimid);
-    [~, kb] = netcdf.inqDim(ncid, siglev_dimid);
-    [~, nt] = netcdf.inqDim(ncid, time_dimid);
-    hyw = zeros(node, kb, nt);
+    hyw = hyw * ones(node, kb, nt);
 end
+
+if (size(hyw,1) ~= node) || (size(hyw,2) ~= kb) || (size(hyw,3) ~= nt)
+    disp(size(hyw))
+    disp([node, kb, nt])
+    error('Wrong size of hyw')
+end 
 
 hyw_varid=netcdf.defVar(ncid, 'hyw', 'float', [node_dimid siglev_dimid time_dimid]);
 netcdf.putAtt(ncid,hyw_varid, 'long_name', 'hydro static vertical velocity');
@@ -54,7 +57,11 @@ netcdf.putAtt(ncid,hyw_varid, 'type', 'data');
 netcdf.endDef(ncid);
 
 for it = 1 : nt
-    netcdf.putVar(ncid, hyw_varid, [1 1 it-1], [Inf Inf 1], hyw(:,:,it));
+    it
+    if (mod(it, 10) == 0)
+        disp(['--->' num2str(it,'%6d') '/' num2str(nt,'%6d')])
+    end 
+    netcdf.putVar(ncid, hyw_varid, [0 0 it-1], [node kb 1], hyw(:,:,it));
 end
 
 netcdf.close(ncid);
