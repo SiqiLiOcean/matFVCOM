b7249cd4a72f9018d9f3eed6ec564e459dbe1c1a
Siqi Li <1990lisiqi@gmail.com>
2024-01-11 10:51:05 -0500

    Update check_grid_type.m

--- check_grid_type.m
+++ check_grid_type.m
@@ -15,15 +15,16 @@
 %==========================================================================
 function type = check_grid_type(x, y)
 
+eps = 1e-7;
 
 xlims = minmax(x);
 ylims = minmax(y);
 type = 'Regional';
-if ylims(1)>=-90 && ylims(2)<=90 && xlims(1)>=0 && xlims(2)<=360
+if ylims(1)>=-90 && abs(ylims(2)-90)<eps && abs(xlims(1))<eps && abs(xlims(2)-360)<eps
     if all(histcounts(x, 0:5:360)>0)
         type = 'Global';
     end
-elseif ylims(1)>=-90 && ylims(2)<=90 && xlims(1)>=-180 && xlims(2)<=180
+elseif ylims(1)>=-90 && abs(ylims(2)-90)<eps && abs(xlims(1)+180)<eps && abs(xlims(2)-180)<eps
     if all(histcounts(x, -180:5:180)>0)
         type = 'Global';
     end   
